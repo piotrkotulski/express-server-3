6e238bb7b03e57b06573e0d4826e8d7950907ae9
piotr kotulski <piotr.kotulski@exorigo-upos.pl>
2024-01-23 22:28:53 +0100

    builded app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 
@@ -8,10 +9,11 @@ const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
 
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,6 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/testimonials', testimonialsRoutes);
 app.use('/api/concerts', concertsRoutes);
 app.use('/api/seats', seatsRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Not found...' });
